50d0fd917ac11fce1b41afec24b42d5d0c581eff
Geralyn Mendall <geralyn.mendall@gmail.com>
2019-10-28 10:37:31 -0400

    Fixed error in javascript file. made quotes gramatically correct.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 var quotes = [
-    'If you cant iterate, then you cant jiterate.',
-    'Necessity is the mother of the impossible.',
-    'You better console.log sooner rather than later or youre gonna get got.',
-    'Youre either Team Camel case or Team Underscore, you cant be both.'
+    "If you can't iterate, then you can't jiterate.",
+    "Necessity is the mother of the impossible.",
+    "You better console.log sooner rather than later or you're gonna get got.",
+    "You're either Team Camel case or Team Underscore, you can't be both."
 ];
 
 function newQuote() {
 var randomNumber = Math.floor(Math.random() * (quotes.length));
-}
 document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
